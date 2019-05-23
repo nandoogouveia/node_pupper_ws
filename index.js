2e0bef9ca64e9fd9da2c0dd7c58251d0e9e1f7cc
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-05-23 21:02:40 +0000

    definindo a função async

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ if (!url) {
     throw "URL argumento 1";
     
 }
+async function run () {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({path: 'screenshot.png'});
+    browser.close();
+}
+run();
